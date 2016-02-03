1c6362d205a4423578bd9d8cf982850f68cf396b
James Kyburz <james.kyburz@gmail.com>
2016-02-03 21:18:54 +0100

    failing test with latest hyperx due to undefined children

--- test/vdom/source.js
+++ test/vdom/source.js
@@ -6,6 +6,7 @@ var title = 'world'
 var wow = [1,2,3]
 var tree = hx`<div>
   <h1 y="ab${1+2}cd">hello ${title}!</h1>
+  <input type="text">
   ${hx`<i>cool</i>`}
   wow
   ${wow.map(function (w, i) {
